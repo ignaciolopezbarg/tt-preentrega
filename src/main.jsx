499906b85c9416a5402eb334e393011e0bc60dfa
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2025-07-03 17:18:18 -0300

    fix 404 routing for Gihub pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
@@ -9,13 +9,13 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-       <BrowserRouter basename={import.meta.env.BASE_URL}>
+       <HashRouter>
         <ThemeProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </StrictMode>
 );
